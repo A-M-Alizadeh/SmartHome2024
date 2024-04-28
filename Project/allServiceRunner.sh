6085f54d8afe8c661655d62e8a0a7fe806927804
Ali <am_alizadeh74@yahoo.com>
2024-04-28 13:56:31 +0200

    small changes - next is docker

--- Project/allServiceRunner.sh
+++ Project/allServiceRunner.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-
 (bash $(pwd)"/CatalogService/serviceRunner.sh")
+
+osascript -e 'tell app "Terminal"
+    do script "influxd"
+end tell'
+
 sleep 3
 (bash $(pwd)"/CommandCenterService/serviceRunner.sh")
 (bash $(pwd)"/DBService/serviceRunner.sh")
